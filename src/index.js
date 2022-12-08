b6447d789c2f7ac9c08496d8b4a9e9c4202e5ec7
saudikramxx <saudikram62@gmail.com>
2022-12-08 17:27:00 +0530

    contacts app

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import contacts from "./contacts";
+
+ReactDOM.render(<div>
+
+
+<h1 className="heading">My Contacts</h1>
+
+<App name = {contacts[0].name}
+ img = {contacts[0].imgURL} 
+ tel={contacts[0].phone} 
+ email ={contacts[0].email} />
+
+
+ <App name = {contacts[1].name}
+ img = {contacts[1].imgURL} 
+ tel={contacts[1].phone} 
+ email ={contacts[1].email} />
+
+
+ <App name = {contacts[2].name}
+ img = {contacts[2].imgURL} 
+ tel={contacts[2].phone} 
+ email ={contacts[2].email} />
+
+
+ </div>,
+ document.getElementById("root"));
+
+
+
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
